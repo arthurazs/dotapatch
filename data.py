55f74eea257024ff4bbdc45d6aafcd01f6ebcacf
Arthur Zopellaro <arthurazsoares@gmail.com>
2016-10-23 04:17:03 -0200

    Refactoring data fetching

---
+++ data.py
@@ -0,0 +1,107 @@
+import ast, os.path
+class HeropediaData (object):
+
+    #CONSTANTS
+    DATA = 'data/'
+    ITEM_DATA = 'itemdata'
+    HERO_DATA = 'herodata'
+    ABILITY_DATA = 'abilitydata'
+
+    #Initialization Functions
+    def _loadFile(self, name):
+        link = 'http://www.dota2.com/jsfeed/heropediadata?feeds=' + name
+        code = requests.get(link)
+        data = code.text.replace('{"' + name + '":', '').replace('}}', '}').replace(':false', ':False').replace(':null', ':None').replace(':true', ':True')
+        dictionary = ast.literal_eval(data)
+        return dictionary
+
+    def _openFile(self, name):
+        with open(self.DATA + name, 'r') as text:
+            data = text.read()
+            dictionary = ast.literal_eval(data)
+            return dictionary
+
+    def _saveFile(self, name, content):
+        with open(DATA + name, 'w') as text:
+            print(content, file=text)
+
+    #Initialization
+    def __init__(self):
+        #Check data folder
+        if not os.path.exists(self.DATA):
+            os.makedirs(self.DATA)
+
+        #Data Initialization
+        if(not os.path.isfile(self.DATA + self.ITEM_DATA)):
+            self._item_dictionary = self._loadFile(self.ITEM_DATA)
+            self._saveFile(self.ITEM_DATA, self._item_dictionary)
+        else:
+            self._item_dictionary = self._openFile(self.ITEM_DATA)
+
+        if(not os.path.isfile(self.DATA + self.HERO_DATA)):
+            self._hero_dictionary = self._loadFile(self.HERO_DATA)
+            self._saveFile(self.HERO_DATA, self._hero_dictionary)
+        else:
+            self._hero_dictionary = self._openFile(self.HERO_DATA)
+
+        if(not os.path.isfile(self.DATA + self.ABILITY_DATA)):
+            self._ability_dictionary = self._loadFile(self.ABILITY_DATA)
+            self._saveFile(self.ABILITY_DATA, self._ability_dictionary)
+        else:
+            self._ability_dictionary = self._openFile(self.ABILITY_DATA)
+
+    def getDictionary(self, name):
+        if name == 'ability':
+            return self._ability_dictionary
+        elif name == 'hero':
+            return self._hero_dictionary
+        elif name == 'item':
+            return self._item_dictionary
+        else:
+            return None
+
+    #Handle name bugs
+    def _checkDname(self, name):
+        if (name.split()[0].lower() == 'drow'):
+            return 'Drow Ranger'
+        if (name.split()[0].lower() == 'nyx\'s'):
+            return 'Nyx Assassin'
+        elif (name.split()[0].lower() == 'smokescreen'):
+            return 'Smoke Screen'
+        elif (name.split()[0].lower() == 'starfall'):
+            return 'Starstorm'
+        return name
+
+    def _checkHurl(self, name):
+        if (name.split()[0].lower() == 'io'):
+            return 'wisp'
+        if (name.split()[0].lower() == 'underlord'):
+            return 'abyssal_underlord'
+        return name
+
+    def _checkItem(self, name):
+        if (name.lower() == 'diffusal_blade_2'):
+            return 'diffusal_blade'
+        return name
+
+    #Default Function
+    def _get_name(self, name, dictionary):
+        for key, value in dictionary.items():
+            length = len(value['dname'].split(' '))
+            if (self._checkDname(' '.join(name[:length])) == value['dname']):
+                return (self._checkItem(key), value)
+        return (None, None)
+
+    #Name Functions
+    def get_item_name(self, name):
+        return self._get_name(name, self._item_dictionary)[0]
+
+    def get_hero_name(self, name):
+        return self._get_name(name, self._hero_dictionary)[0]
+
+    def get_ability_hero(self, name):
+        key, value = self._get_name(name, self._ability_dictionary)
+        if key:
+            return self._checkHurl(value['hurl'].lower())
+        else:
+            return None
--- patch.py
+++ patch.py
@@ -1,64 +1,21 @@
 #!/usr/bin/env python3
 #coding: utf-8
-import requests, ast, os.path, argparse
+import requests, os.path, argparse
 from collections import defaultdict
 from model import Html
+from data import HeropediaData
 
 parser = argparse.ArgumentParser(description="This software formats a Dota2' changelog text into HTML.")
 parser.add_argument('--file', '-f', action='store', help="changelog to be formated", required = True, dest = 'file')
 parser.add_argument('--version', '-v', action='version', version='%(prog)s: 0.0v')
 args = parser.parse_args()
 
-#CONSTANTS
-DATA = 'data/'
+#CONSTANT
 CHANGELOG = 'changelogs/'
-ITEM_DATA = 'itemdata'
-HERO_DATA = 'herodata'
-ABILITY_DATA = 'abilitydata'
 
-#Initialization Functions
-def loadFile(name):
-    link = 'http://www.dota2.com/jsfeed/heropediadata?feeds=' + name
-    code = requests.get(link)
-    data = code.text.replace('{"' + name + '":', '').replace('}}', '}').replace(':false', ':False').replace(':null', ':None').replace(':true', ':True')
-    dictionary = ast.literal_eval(data)
-    return dictionary
-
-def openFile(name):
-    with open(DATA + name, 'r') as text:
-        data = text.read()
-        dictionary = ast.literal_eval(data)
-        return dictionary
-
-def saveFile(name, content):
-    with open(DATA + name, 'w') as text:
-        print(content, file=text)
-
-#Check folders
-if not os.path.exists(DATA):
-    os.makedirs(DATA)
-
+#Check changelog folder
 if not os.path.exists(CHANGELOG):
     os.makedirs(CHANGELOG)
-
-#Data Initialization
-if(not os.path.isfile(DATA + ITEM_DATA)):
-    item_dictionary = loadFile(ITEM_DATA)
-    saveFile(ITEM_DATA, item_dictionary)
-else:
-    item_dictionary = openFile(ITEM_DATA)
-
-if(not os.path.isfile(DATA + HERO_DATA)):
-    hero_dictionary = loadFile(HERO_DATA)
-    saveFile(HERO_DATA, hero_dictionary)
-else:
-    hero_dictionary = openFile(HERO_DATA)
-
-if(not os.path.isfile(DATA + ABILITY_DATA)):
-    ability_dictionary = loadFile(ABILITY_DATA)
-    saveFile(ABILITY_DATA, ability_dictionary)
-else:
-    ability_dictionary = openFile(ABILITY_DATA)
 
 #Open changelog
 if os.path.isfile(CHANGELOG+args.file):
@@ -73,51 +30,7 @@ if os.path.isfile(CHANGELOG+args.file):
         lines = lines[2:]
         initialLineCount = len(lines)
 
-    #Handle name bugs
-    def checkDname(name):
-        if (name.split()[0].lower() == 'drow'):
-            return 'Drow Ranger'
-        if (name.split()[0].lower() == 'nyx\'s'):
-            return 'Nyx Assassin'
-        elif (name.split()[0].lower() == 'smokescreen'):
-            return 'Smoke Screen'
-        elif (name.split()[0].lower() == 'starfall'):
-            return 'Starstorm'
-        return name
-
-    def checkHurl(name):
-        if (name.split()[0].lower() == 'io'):
-            return 'wisp'
-        if (name.split()[0].lower() == 'underlord'):
-            return 'abyssal_underlord'
-        return name
-
-    def checkItem(name):
-        if (name.lower() == 'diffusal_blade_2'):
-            return 'diffusal_blade'
-        return name
-
-    #Default Function
-    def get_name(name, dictionary):
-        for key, value in dictionary.items():
-            length = len(value['dname'].split(' '))
-            if (checkDname(' '.join(name[:length])) == value['dname']):
-                return (checkItem(key), value)
-        return (None, None)
-
-    #Name Functions
-    def get_item_name(name):
-        return get_name(name, item_dictionary)[0]
-
-    def get_hero_name(name):
-        return get_name(name, hero_dictionary)[0]
-
-    def get_ability_hero(name):
-        key, value = get_name(name, ability_dictionary)
-        if key:
-            return checkHurl(value['hurl'].lower())
-        else:
-            return None
+    data = HeropediaData()
 
     #Organize changelog
     item = defaultdict(list)
@@ -125,19 +38,19 @@ if os.path.isfile(CHANGELOG+args.file):
     ability = defaultdict(list)
     for line in lines[:]:
         names = line.split(' ')[:3]
-        found_ability = get_ability_hero(names)
+        found_ability = data.get_ability_hero(names)
         if found_ability:
             ability[found_ability].append(line)
             lines.remove(line)
 
     for line in lines[:]:
         names = line.split(' ')[:3]
-        found_hero = get_hero_name(names)
+        found_hero = data.get_hero_name(names)
         if found_hero:
             hero[found_hero].append(line)
             lines.remove(line)
         else:
-            found_item = get_item_name(names)
+            found_item = data.get_item_name(names)
             if found_item:
                 item[found_item].append(line)
                 lines.remove(line)
