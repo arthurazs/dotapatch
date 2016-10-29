6717753711df925cca31c8772bc9a6e195ff8e57
Arthur Zopellaro <arthurazsoares@gmail.com>
2016-10-28 23:14:37 -0200

    Add 'themes/default' to 'model.py'
    'model.py' uses the 'default' file to create the HTML now.
    See #3

--- model.py
+++ model.py
@@ -1,46 +1,22 @@
 from __future__ import print_function
 from data import HeropediaData
+import ast
 
 class Html (object):
 
+    _THEMES_FOLDER = 'themes/'
+
+    def _read_theme(self, theme_name):
+        with open(self._THEMES_FOLDER + theme_name, 'r') as theme_file:
+            theme_dictionary = ast.literal_eval(theme_file.read())
+        return theme_dictionary
+
     #Initialization
-    def __init__(self, title):
+    def __init__(self, title, theme = 'default'):
         self._title = title
+        self._theme_dictionary = self._read_theme(theme)
         self._bgStyle = False
-        self._content = r'''<!DOCTYPE html>
-<html>
-    <head>
-        <title>dota2patches {0} by @arthurazs</title>
-
-        <link rel="shortcut icon" href="favicon.ico" />
-
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=9" />
-
-        <link href="css/global.css" rel="stylesheet" type="text/css" >
-        <link href="css/global_english.css" rel="stylesheet" type="text/css" >
-        <link href="css/changelog.css" rel="stylesheet" type="text/css" >
-
-        <script type="text/javascript" src="javascript/jquery-1.7.1.min.js"></script>
-        <script type="text/javascript">$J = jQuery;</script>
-        <script src="javascript/fetch_heropediadata.js"></script>
-    </head>
-
-    <body>
-        <div class="Container" id="KeyArt">
-            <div class="Inner">
-                <a href="http://www.dota2.com/"><img src="css/images/logo.png" /></a>
-            </div>
-        </div>
-
-        <a class="HiddenAnchor" name="GameplayHeader"> </a>
-        <div class="Container" id="Header">
-            <div class="Inner" style="text-align:center">
-                <h1>Gameplay Update {0}</h1>
-                <h3><a href="index.html">Previous changelogs</a></h3>
-            </div>
-        </div>
-'''.format(self._title)
+        self._content = self._theme_dictionary['OPEN_HTML'].format(title = self._title)
 
     #Default function for adding content
     def _addContent(self, text):
@@ -49,84 +25,47 @@ class Html (object):
     #Add GENERAL Contents
     def addGeneral(self, lines):
         if(lines):
-            self._addContent(r'''
-        <div class="Container RepeatY BGStyle{}">
-            <div class="Inner">
-                <h3>General</h3>'''.format(int(self._bgStyle)))
+            self._addContent(self._theme_dictionary['OPEN_GENERAL'].format(int(self._bgStyle)))
+            self._addContent(self._theme_dictionary['OPEN_GENERAL_UL'])
 
-            self._addContent(r'''
-
-                    <ul>''')
             for line in lines:
-                self._addContent(r'''
-                    <li>{}</li>'''.format(line))
-            self._addContent(r'''
-                    </ul>
-''')
+                self._addContent(self._theme_dictionary['GENEREAL_LI'].format(line))
 
-            self._addContent(r'''
-            </div>
-        </div>''')
+            self._addContent(self._theme_dictionary['CLOSE_GENERAL_UL'])
+            self._addContent(self._theme_dictionary['CLOSE_GENERAL'])
             self._bgStyle = not self._bgStyle
 
     #Add ITEMS Contents
     def addItems(self, dictionary):
         if(dictionary):
-            self._addContent(r'''
-        <div class="Container RepeatY BGStyle{}" id="Items">
-            <div class="Inner">
-                <h3>Item changes</h3>'''.format(int(self._bgStyle)))
+            self._addContent(self._theme_dictionary['OPEN_ITEMS'].format(int(self._bgStyle)))
 
             for key, values in sorted(dictionary.items()):
-                self._addContent(r'''
-
-                    [[{}]]
-                    <ul>'''.format(key))
+                self._addContent(self._theme_dictionary['OPEN_ITEMS_UL'].format(key))
                 for value in values:
-                    self._addContent(r'''
-                        <li>{}</li>'''.format(value))
-                self._addContent(r'''
-                    </ul>
-''')
+                    self._addContent(self._theme_dictionary['ITEMS_LI'].format(value))
+                self._addContent(self._theme_dictionary['CLOSE_ITEMS_UL'])
 
-            self._addContent(r'''
-            </div>
-        </div>''')
+            self._addContent(self._theme_dictionary['CLOSE_ITEMS'])
             self._bgStyle = not self._bgStyle
 
     #Add HERO Contents
     def addHeros(self, dictionary):
         if(dictionary):
-            self._addContent(r'''
-        <div class="Container RepeatY BGStyle{}" id="Heroes">
-             <div class="Inner">
-                <h3>Heroes</h3>'''.format(int(self._bgStyle)))
+            self._addContent(self._theme_dictionary['OPEN_HEROES'].format(style = int(self._bgStyle)))
 
             for key, values in sorted(dictionary.items(), key=HeropediaData.sort):
-                self._addContent(r'''
-
-                    [[{}]]
-                    <ul>'''.format(key))
+                self._addContent(self._theme_dictionary['OPEN_HEROES_UL'].format(key = key))
                 for value in values:
-                    self._addContent(r'''
-                        <li>{}</li>'''.format(value))
-                self._addContent(r'''
-                    </ul>
-''')
+                    self._addContent(self._theme_dictionary['HEROES_LI'].format(value = value))
+                self._addContent(self._theme_dictionary['CLOSE_HEROES_UL'])
 
-            self._addContent(r'''
-            </div>
-        </div>''')
+            self._addContent(self._theme_dictionary['CLOSE_HEROES'])
             self._bgStyle = not self._bgStyle
 
     #Closes the HTML
     def close(self):
-        self._addContent(r'''
-        <div class="Container" id="Footer">
-            <div class="Inner"></div>
-        </div>
-    </body>
-</html>''')
+        self._addContent(self._theme_dictionary['CLOSE_HTML'])
 
     #Returns HTML Content
     def getContent(self):
