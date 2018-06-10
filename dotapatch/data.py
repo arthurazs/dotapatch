daac7a2da310ecc613b11cc812c25a8cb7c60ecc
Arthur Zopellaro <arthurazsoares@gmail.com>
2018-06-10 15:06:36 -0300

    Add Eul's to item names

--- dotapatch/data.py
+++ dotapatch/data.py
@@ -40,6 +40,7 @@ class HeropediaData(object):
         'blink': 'blink_dagger', 'travel_boots': 'boots_of_travel',
         'ward_observer': 'observer_ward', 'vladmir': "vladmir's_offering",
         'heavens_halberd': "heaven's_halberd", 'ward_sentry': 'sentry_ward',
+        'cyclone': "eul's_scepter_of_divinity"
         # 'combo_breaker': 'aeon_disk'
     }
 
