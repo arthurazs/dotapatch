484f892ad8997861b73757e7981c1d59bf668e35
Arthur Zopellaro <arthurazsoares@gmail.com>
2018-09-02 13:56:04 -0300

    Add scythe of vyse to ITEM_NAME

--- dotapatch/data.py
+++ dotapatch/data.py
@@ -40,7 +40,8 @@ class HeropediaData(object):
         'blink': 'blink_dagger', 'travel_boots': 'boots_of_travel',
         'ward_observer': 'observer_ward', 'vladmir': "vladmir's_offering",
         'heavens_halberd': "heaven's_halberd", 'ward_sentry': 'sentry_ward',
-        'cyclone': "eul's_scepter_of_divinity", 'tpscroll': 'tp_scroll'
+        'cyclone': "eul's_scepter_of_divinity", 'tpscroll': 'tp_scroll',
+        'sheepstick': 'scythe_of_vyse'
         # 'combo_breaker': 'aeon_disk'
     }
 
