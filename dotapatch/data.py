58f78c3226bdc3d1777bd41dc87f8cad964e0655
Arthur Zopellaro <arthurazsoares@gmail.com>
2019-01-29 18:54:28 -0200

    Add drums

--- dotapatch/data.py
+++ dotapatch/data.py
@@ -41,7 +41,8 @@ class HeropediaData(object):
         'ward_observer': 'observer_ward', 'vladmir': "vladmir's_offering",
         'heavens_halberd': "heaven's_halberd", 'ward_sentry': 'sentry_ward',
         'cyclone': "eul's_scepter_of_divinity", 'tpscroll': 'tp_scroll',
-        'sheepstick': 'scythe_of_vyse', 'branches': 'iron_branch'
+        'sheepstick': 'scythe_of_vyse', 'branches': 'iron_branch',
+        'ancient_janggo': 'drum_of_endurance'
         # 'combo_breaker': 'aeon_disk'
     }
 
